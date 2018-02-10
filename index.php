74734ee812f0acdeb4b5d45b0928afecc1d2f6e3
Pavel Obdržálek <pobdr@centrum.cz>
2018-02-10 15:41:41 +0100

    predelano do funkce

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
+<?php
+//README: upravuj tady, dolů moc nehrab
+
+$nadpis = "Matfyz FAQ";
+
+$podnadpis = "To, na co jste se nás už nejednou ptali.";
+
+$nazvy = array //usage: array("Nadpis sekce", "unikatni identifikatro (bude slouzit i jako predpona pro nedefinovana jmena linku; nesmi obsahovat podtrzitko)", "cesta k souboru s otazkami"); Proc to nefunguje? Bacha, za posledni sekci nesmi carka...
+  (
+  array("Obecné dotazy","obecne","obecne_dotazy.csv"),
+  array("Fyzikální dotazy","fyzikalni","fyzikalni_dotazy.csv"),
+  array("Informatické dotazy","informaticke","informaticke_dotazy.csv"),
+  array("Matematické dotazy","matematicke","matematicke_dotazy.csv")
+  );
+?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>Matfyz FAQ</title>
+  <title><?php echo $nadpis;?></title>
   <base target="_parent">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css" type="text/css">
@@ -48,7 +63,7 @@
 <body onload="scroll_to_hash();">
 
 
-<section class="header11 cid-qv5BLEQHXO" id="header11-56" data-rv-view="2085">
+<section class="header11 cid-qv5BLEQHXO" id="header" data-rv-view="2085">
 
     <!-- Block parameters controls (Blue "Gear" panel) -->
     
@@ -58,235 +73,63 @@
     <div class="container align-left">
         <div class="media-container-column mbr-white col-md-12">
             
-            <h1 class="mbr-section-title py-3 mbr-fonts-style display-1">Matfyz FAQ</h1>
+            <h1 class="mbr-section-title py-3 mbr-fonts-style display-1"><?php echo $nadpis;?></h1>
             
-            <h3 class="mbr-section-title py-3 mbr-fonts-style display-2">To, na co jste se nás už nejednou ptali.</h3>
+            <h3 class="mbr-section-title py-3 mbr-fonts-style display-2"><?php echo $podnadpis;?></h3>
         </div>
     </div>
 
     
 </section>
 
-
-<section class="toggle1 cid-qv5Aju6kt2" id="dotaz_obecne" data-rv-view="2091">
-
+<?php
+function vypisSekci($nadpis, $id, $cesta) {
+    $csv = array_map('str_getcsv', file($cesta));
+    array_walk($csv, function(&$a) use ($csv) {
+      $a = array_combine($csv[0], $a);
+    });
+    array_shift($csv); # remove column header
     
-
-    
-        <div class="container" role="tablist">
+    echo '<section class="toggle1 cid-qv5Aju6kt2" id="dotaz_'.$id.'" data-rv-view="2091">
+      <div class="container" role="tablist">
             <div class="media-container-row">
                 <div class="col-12 col-md-12">
                     <div class="section-head text-center space30">
-                       <h2 class="mbr-section-title pb-5 mbr-fonts-style display-2">
-                            Obecné dotazy</h2>
+                       <h2 class="mbr-section-title pb-5 mbr-fonts-style display-2">'.$nadpis.'</h2>
                     </div>
                     <div class="clearfix"></div>
-                    <div id="bootstrap-toggle-obecne" class="toggle-panel accordionStyles tab-content">
-                    <?php
-    
-    $file_obecne = "obecne_dotazy.csv";
-    $csv_obecne = array_map('str_getcsv', file($file_obecne));
-    array_walk($csv_obecne, function(&$a) use ($csv_obecne) {
-      $a = array_combine($csv_obecne[0], $a);
-    });
-    array_shift($csv_obecne); # remove column header
-    echo "<br />";
-    foreach($csv_obecne as $i => $trojice_obecne)
+                    <div id="bootstrap-toggle-'.$id.'" class="toggle-panel accordionStyles tab-content"><br />';
+    foreach($csv as $i => $trojice)
       {
-        if (empty($trojice_obecne["link"]))
+        if (empty($trojice["link"]))
           {                                        
-            $trojice_obecne["link"] ="obecne-".($i+1);
+            $trojice["link"] =$id."-".($i+1);
           }
-                  echo '<div class="card">
-                            <div class="card-header" role="tab" id="dotaz_'.$trojice_obecne["link"].'">
-                                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-parent="#accordion" data-core="" href="#'.$trojice_obecne["link"].'" id="btn_'.$trojice_obecne["link"].'" aria-expanded="false" aria-controls="'.$trojice_obecne["link"].'">
-                                    <h4 class="mbr-fonts-style display-6">
-                                        <span class="sign mbr-iconfont mbri-arrow-down inactive" id="question_'.$trojice_obecne["link"].'"></span>'.$trojice_obecne["Q"].
-                                    '</h4>
-                                </a>
-                            </div>
-                            <div id="'.$trojice_obecne["link"].'" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="dotaz_'.$trojice_obecne["link"].'">
-                                <div class="panel-body px-4 pt-4">
-                                    <p class="mbr-fonts-style panel-text display-7">'.$trojice_obecne["A"].
-                                '</div>
-                                <span class="pb-4 mbri-link-chain" style="float: right;padding-bottom:5px;" onclick="copyToClipboard(\'#'.$trojice_obecne["link"].'\')" title="odkaz"></span>
-                            </div>
-                        </div>';
-      }
-    ?>                             
-                        
-                        
-                        
+          echo '<div class="card">
+                    <div class="card-header" role="tab" id="dotaz_'.$trojice["link"].'">
+                        <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-parent="#accordion" data-core="" href="#'.$trojice["link"].'" id="btn_'.$trojice["link"].'" aria-expanded="false" aria-controls="'.$trojice["link"].'">
+                            <h4 class="mbr-fonts-style display-6">
+                                <span class="sign mbr-iconfont mbri-arrow-down inactive" id="question_'.$trojice["link"].'"></span>'.$trojice["Q"].
+                            '</h4>
+                        </a>
                     </div>
+                    <div id="'.$trojice["link"].'" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="dotaz_'.$trojice["link"].'">
+                        <div class="panel-body px-4 pt-4">
+                            <p class="mbr-fonts-style panel-text display-7">'.$trojice["A"].
+                        '</div>
+                        <span class="pb-4 mbri-link-chain" style="float: right;padding-bottom:5px;" onclick="copyToClipboard(\'#'.$trojice["link"].'\')" title="odkaz"></span>
+                    </div>
+                </div>';
+      }
+   echo '</div>
                 </div>
             </div>
         </div>
-</section>
-
-<section class="toggle1 cid-qv5Aju6kt2" id="dotaz_fyzika" data-rv-view="2091">
-
-    
-
-    
-        <div class="container" role="tablist">
-            <div class="media-container-row">
-                <div class="col-12 col-md-12">
-                    <div class="section-head text-center space30">
-                       <h2 class="mbr-section-title pb-5 mbr-fonts-style display-2">
-                            Fyzikální dotazy</h2>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div id="bootstrap-toggle-fyzikalni" class="toggle-panel accordionStyles tab-content">
-                    <?php
-    
-    $file_fyzikalni = "fyzikalni_dotazy.csv";
-    $csv_fyzikalni = array_map('str_getcsv', file($file_fyzikalni));
-    array_walk($csv_fyzikalni, function(&$a) use ($csv_fyzikalni) {
-      $a = array_combine($csv_fyzikalni[0], $a);
-    });
-    array_shift($csv_fyzikalni); # remove column header
-    echo "<br />";
-    foreach($csv_fyzikalni as $i => $trojice_fyzikalni)
-      {
-        if (empty($trojice_fyzikalni["link"]))
-          {                                        
-            $trojice_fyzikalni["link"] ="fyzikalni-".($i+1);
-          }
-                  echo '<div class="card">
-                            <div class="card-header" role="tab" id="dotaz_'.$trojice_fyzikalni["link"].'">
-                                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-parent="#accordion" data-core="" href="#'.$trojice_fyzikalni["link"].'" id="btn_'.$trojice_fyzikalni["link"].'" aria-expanded="false" aria-controls="'.$trojice_fyzikalni["link"].'">
-                                    <h4 class="mbr-fonts-style display-6">
-                                        <span class="sign mbr-iconfont mbri-arrow-down inactive" id="question_'.$trojice_fyzikalni["link"].'"></span>'.$trojice_fyzikalni["Q"].
-                                    '</h4>
-                                </a>
-                            </div>
-                            <div id="'.$trojice_fyzikalni["link"].'" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="dotaz_'.$trojice_fyzikalni["link"].'">
-                                <div class="panel-body px-4 pt-4">
-                                    <p class="mbr-fonts-style panel-text display-7">'.$trojice_fyzikalni["A"].
-                                '</div>
-                                <span class="pb-4 mbri-link-chain" style="float: right;padding-bottom:5px;" onclick="copyToClipboard(\'#'.$trojice_fyzikalni["link"].'\')" title="odkaz"></span>
-                            </div>
-                        </div>';
-      }
-    ?>                  
-                        
-                        
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-</section>
-
-<section class="toggle1 cid-qv5Aju6kt2" id="dotaz_informatika" data-rv-view="2091">
-
-    
-
-    
-        <div class="container" role="tablist">
-            <div class="media-container-row">
-                <div class="col-12 col-md-12">
-                    <div class="section-head text-center space30">
-                       <h2 class="mbr-section-title pb-5 mbr-fonts-style display-2">
-                            Informatické dotazy</h2>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div id="bootstrap-toggle-informaticke" class="toggle-panel accordionStyles tab-content">
-                    <?php
-    
-    $file_informaticke = "informaticke_dotazy.csv";
-    $csv_informaticke = array_map('str_getcsv', file($file_informaticke));
-    array_walk($csv_informaticke, function(&$a) use ($csv_informaticke) {
-      $a = array_combine($csv_informaticke[0], $a);
-    });
-    array_shift($csv_informaticke); # remove column header
-    echo "<br />";
-    foreach($csv_informaticke as $i => $trojice_informaticke)
-      {
-        if (empty($trojice_informaticke["link"]))
-          {                                        
-            $trojice_informaticke["link"] ="informaticke-".($i+1);
-          }
-                  echo '<div class="card">
-                            <div class="card-header" role="tab" id="dotaz_'.$trojice_informaticke["link"].'">
-                                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-parent="#accordion" data-core="" href="#'.$trojice_informaticke["link"].'" id="btn_'.$trojice_informaticke["link"].'" aria-expanded="false" aria-controls="'.$trojice_informaticke["link"].'">
-                                    <h4 class="mbr-fonts-style display-6">
-                                        <span class="sign mbr-iconfont mbri-arrow-down inactive" id="question_'.$trojice_informaticke["link"].'"></span>'.$trojice_informaticke["Q"].
-                                    '</h4>
-                                </a>
-                            </div>
-                            <div id="'.$trojice_informaticke["link"].'" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="dotaz_'.$trojice_informaticke["link"].'">
-                                <div class="panel-body px-4 pt-4">
-                                    <p class="mbr-fonts-style panel-text display-7">'.$trojice_informaticke["A"].
-                                '</div>
-                                    <span class="pb-4 mbri-link-chain" style="float: right;padding-bottom:5px;" onclick="copyToClipboard(\'#'.$trojice_informaticke["link"].'\')" title="odkaz"></span> 
-                            </div>
-                        </div>';
-      }
-    ?>                  
-                        
-                        
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-</section>
-
-<section class="toggle1 cid-qv5Aju6kt2" id="dotaz_matematika" data-rv-view="2091">
-
-    
-
-    
-        <div class="container" role="tablist">
-            <div class="media-container-row">
-                <div class="col-12 col-md-12">
-                    <div class="section-head text-center space30">
-                       <h2 class="mbr-section-title pb-5 mbr-fonts-style display-2">
-                            Matematické dotazy</h2>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div id="bootstrap-toggle-matematicke" class="toggle-panel accordionStyles tab-content">
-                    <?php
-    
-    $file_matematicke = "matematicke_dotazy.csv";
-    $csv_matematicke = array_map('str_getcsv', file($file_matematicke));
-    array_walk($csv_matematicke, function(&$a) use ($csv_matematicke) {
-      $a = array_combine($csv_matematicke[0], $a);
-    });
-    array_shift($csv_matematicke); # remove column header
-    echo "<br />";
-    foreach($csv_matematicke as $i => $trojice_matematicke)
-      {
-        if (empty($trojice_matematicke["link"]))
-          {                                        
-            $trojice_matematicke["link"] ="matematicke-".($i+1);
-          }
-                  echo '<div class="card">
-                            <div class="card-header" role="tab" id="dotaz_'.$trojice_matematicke["link"].'">
-                                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-parent="#accordion" data-core="" href="#'.$trojice_matematicke["link"].'" id="btn_'.$trojice_matematicke["link"].'" aria-expanded="false" aria-controls="'.$trojice_matematicke["link"].'">
-                                    <h4 class="mbr-fonts-style display-6">
-                                        <span class="sign mbr-iconfont mbri-arrow-down inactive" id="question_'.$trojice_matematicke["link"].'"></span>'.$trojice_matematicke["Q"].
-                                    '</h4>
-                                </a>
-                            </div>
-                            <div id="'.$trojice_matematicke["link"].'" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="dotaz_'.$trojice_matematicke["link"].'">
-                                <div class="panel-body px-4 pt-4">
-                                    <p class="mbr-fonts-style panel-text display-7">'.$trojice_matematicke["A"].
-                                '</div>
-                                <span class="pb-4 mbri-link-chain" style="float: right;padding-bottom:5px;" onclick="copyToClipboard(\'#'.$trojice_matematicke["link"].'\')" title="odkaz"></span>
-                            </div>
-                        </div>';
-      }
-    ?>            
-                        
-                        
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-</section>
+</section>';
+}
+  
+foreach($nazvy as $nazev){vypisSekci($nazev[0], $nazev[1], $nazev[2]);}
+?>
 
 
 <section class="header11 cid-qv5BLEQHXO" id="disclaimer" data-rv-view="2085">
@@ -302,12 +145,12 @@
 <br />
 <br />
 <br />
-<section class="cid-qvLq468qgu" id="footer6-d9" data-rv-view="2128" style="position:absolute; bottom:0%; width:100%">
+<section class="cid-qvLq468qgu" id="footer" data-rv-view="2128" style="position:absolute; bottom:0%; width:100%">
     <div class="container">
         <div class="media-container-row align-center mbr-white">
             <div class="col-12">
                 <h6 class="mbr-text mb-0 mbr-fonts-style display-7" style="font-weight:normal;">
-                    &copy; Content copyright 2018<?=(date('Y')>2018?' - '.date('Y'):'')?>: <a href="http://pobdr.matfyz.cz" class="text-white">Pavel Obdržálek</a>, <a href="http://www.ms.mff.cuni.cz/~yaghoboa/" class="text-white">Anna Yaghobová</a>, Míra Štochel, <a href="http://petrroll.cz/" class="text-white">Petr Houška</a>, Petra Hoffmannová<br />a spousta dotazovatelů z řad studentů…
+                    &copy; Content copyright 2018<?=(date('Y')>2018?' - '.date('Y'):'')?>: <a href="http://pobdr.matfyz.cz" class="text-white">Pavel Obdržálek</a>, <a href="http://www.ms.mff.cuni.cz/~yaghoboa/" class="text-white">Anna Yaghobová</a>, Míra Štochel, Petr Houška, Petra Hoffmannová, Peter Korcsok<br />a spousta dotazovatelů z řad studentů…
                 </h6>
                 Pomohlo ti to? Kup nám třeba pivko! <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
